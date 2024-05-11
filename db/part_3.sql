d76e53e7bdc31d046a3ab40c7a5354276a4db900
sxexesx <sxexesx@gmail.com>
2024-05-11 19:07:59 +0300

    updates with sql

--- db/part_3.sql
+++ db/part_3.sql
@@ -57,3 +57,58 @@ FROM o.Sales;
 -- получение разницы дат
 SELECT DATEDIFF(yy, '2017-01-01', '2018-01-01') AS YearDiff;
 SELECT DATEDIFF(dd, '2017-01-01', '2018-01-01') AS DayDiff;
+
+-- --------------------------------------------------------------
+-- простая вставка
+INSERT INTO warehouse.colors
+(colorId, colorName, LastEditedBy)
+VALUES (NEXTVAL('Sequences.colors_id_seq'), 'Ohra', 1);
+
+-- копирование через SELECT
+SELECT first_name, last_name
+INTO actor_copy
+FROM actor
+WHERE actor.actor_id < 10;
+
+-- MSSQL
+INSERT INTO category (category_id, name, last_update)
+    OUTPUT category_id, name, last_update
+    INTO category_copy(category_id, name, last_update)
+VALUES (NEXTVAL(category_category_id_seq), 'trash movie', now()),
+       (NEXTVAL(category_category_id_seq), 'unknown', now());
+
+-- MSSQL
+BEGIN transaction;
+INSERT INTO category (category_id, name, last_update)
+VALUES (NEXTVAL(category_category_id_seq), 'trash movie', now());
+COMMIT;
+
+INSERT INTO category_temp (category_id, name, last_update)
+SELECT name, last_update
+FROM category
+WHERE category_id > 10;
+
+-- простой UPDATE
+UPDATE People
+SET PhoneNumber = '799922234567',
+    FaxNumber   = '799922236677'
+WHERE PersonId = 3;
+
+-- UPDATE с подзапросом
+UPDATE People
+SET FirstSale = (SELECT MIN(Invoice)
+                 FROM Invoices AS I
+                 WHERE People.PersonId = I.SalesPersonId);
+
+-- UPDATE всей таблицы
+UPDATE People
+SET PersonSale  = NULL,
+    PersonSale2 = NULL;
+
+-- DELETE с условием
+DELETE FROM Colors
+WHERE ColorName like '%2%';
+
+-- C Exist
+DELETE FROM Colors
+WHERE EXISTS (SELECT * FROM new_colors);
--- db/part_4.md
+++ db/part_4.md
@@ -1,11 +1,48 @@
-## DML (Insert, Update, Delete, Merge)
+### JOIN
+Аналог горизонтального соединения
 
-### INSERT
-[примеры](part_4.sql)
+<div>
+  <img width="500" height="500" src="src/img09.png" alt="">
+</div>
 
-1. ORDER в INSERT не гарантирует порядок вставки
-2. SELECT * INTO переносит не только значения, но и ПК
-3. Указывать поля в INSERT необходимо для того, чтобы сохранялся порядок вставки
+`CROSS JOIN` - декартово произведение(каждой строчке одной таблицы сопоставляем каждую строчку другой)  
+Пример:
+`FROM t1 CROSS JOIN t2`  
+Старый синтаксис (SQL-89): `FROM t1, t2`
 
-### UPDATE
-[примеры](part_4.sql)
+<div>
+  <img width="300" height="320" src="src/img05.png" alt="">
+</div>
+
+`INNER JOIN` - декартово произведение + фильтрация
+Пример:
+`FROM t1 [INNER] JOIN t2 on t1.id = t2.id`
+
+<div>
+  <img width="300" height="320" src="src/img06.png" alt="">
+</div>
+
+`LEFT/RIGHT JOIN` - INNER JOIN + внешние строки
+Пример:
+`FROM t1 LEFT [OUTER] JOIN t2 on t1.id = t2.id`
+
+<div>
+  <img width="300" height="320" src="src/img07.png" alt="">
+</div>
+
+`FULL JOIN` - LEFT + RIGHT JOIN
+Пример:
+`FROM t1 FULL [OUTER] JOIN t2 on t1.id = t2.id`
+
+Рекомендуется делать индекс по тем колонкам, по которым делаем JOIN.
+
+### UNION
+Аналог вертикального соединения.  
+`UNION ALL` дублирует одинаковые строки.
+
+`UNION` - это `UNION ALL` + `DISTINCT`
+
+<div>
+  <img width="500" height="240" src="src/img08.png" alt="">
+</div>
+
