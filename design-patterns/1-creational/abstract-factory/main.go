a9ad7c0844e58eb44352ef4b3105b44b293d4709
sxexesx <sxexesx@gmail.com>
2024-05-29 13:44:22 +0300

    updates

--- design-patterns/1-creational/abstract-factory/main.go
+++ design-patterns/1-creational/abstract-factory/main.go
@@ -1,11 +1,48 @@
 package abstractfactory
 
-import "fmt"
+type IAbstractFactory interface {
+	CreateButton() IButton
+}
+
+func getUI(osType string) IAbstractFactory {
+	if osType == "win" {
+		return &WinFactory{}
+	}
+	if osType == "mac" {
+		return &MacFactory{}
+	}
+	return nil
+}
+
+// WinFactory concrete fabric
+type WinFactory struct {
+}
+
+func (w *WinFactory) CreateButton() IButton {
+	return &WinButton{
+		Button: Button{
+			height: 100,
+			width:  100,
+		},
+	}
+}
+
+// MacFactory concrete fabric
+type MacFactory struct {
+}
+
+func (m *MacFactory) CreateButton() IButton {
+	return &MacButton{
+		Button: Button{
+			height: 80,
+			width:  80,
+		},
+	}
+}
 
 type IButton interface {
 	getHeight() int
 	setHeight(height int)
-
 	getWidth() int
 	setWidth(width int)
 }
@@ -15,103 +52,21 @@ type Button struct {
 	width  int
 }
 
-func (m *Button) getHeight() int       { return m.height }
-func (m *Button) setHeight(height int) { m.height = height }
-func (m *Button) getWidth() int        { return m.width }
-func (m *Button) setWidth(width int)   { m.width = width }
+func (b *Button) getHeight() int       { return b.height }
+func (b *Button) setHeight(height int) { b.height = height }
+func (b *Button) getWidth() int        { return b.width }
+func (b *Button) setWidth(width int)   { b.width = width }
 
-type ICheckbox interface {
-	getHeight() int
-	setHeight(height int)
-
-	getWidth() int
-	setWidth(width int)
+type WinButton struct {
+	Button
 }
 
-type Checkbox struct {
-	height int
-	width  int
-}
-
-func (c *Checkbox) getHeight() int       { return c.height }
-func (c *Checkbox) setHeight(height int) { c.height = height }
-func (c *Checkbox) getWidth() int        { return c.width }
-func (c *Checkbox) setWidth(width int)   { c.width = width }
-
-type MacButton struct{}
-
-func (m *MacButton) createButton() IButton {
-	return &Button{
-		height: 100,
-		width:  150,
-	}
-}
-func (m *MacButton) createCheckbox() ICheckbox {
-	return &Checkbox{
-		height: 20,
-		width:  20,
-	}
-}
-
-type WinButton struct{}
-
-func (m *WinButton) createButton() IButton {
-	return &Button{
-		height: 80,
-		width:  100,
-	}
-}
-func (m *WinButton) createCheckbox() ICheckbox {
-	return &Checkbox{
-		height: 25,
-		width:  25,
-	}
-}
-
-// func (m *MacButton) getHeight() int       { return m.height }
-// func (m *MacButton) setHeight(height int) { m.height = height }
-// func (m *MacButton) getWidth() int        { return m.width }
-// func (m *MacButton) setWidth(width int)   { m.width = width }
-
-// type WinButton struct {
-// 	height int
-// 	width  int
-// }
-
-// func (m *MacButton) getHeight() int       { return m.height }
-// func (m *MacButton) setHeight(height int) { m.height = height }
-// func (m *MacButton) getWidth() int        { return m.width }
-// func (m *MacButton) setWidth(width int)   { m.width = width }
-
-type IAbstractFactory interface {
-	createButton() IButton
-	createCheckbox() ICheckbox
-}
-
-type WinFactory struct{}
-
-func (w *WinFactory) createButton() {
-}
-
-func (w *WinFactory) createCheckbox() {
-}
-
-type MacFactory struct{}
-
-func (m *MacFactory) createButton() IButton {
-}
-
-func (m *MacFactory) createCheckbox() ICheckbox {
-}
-
-func getUI(osType string) (*IAbstractFactory, error) {
-	if osType == "win" {
-		return &WinFactory{}
-	}
-	if osType == "mac" {
-
-	}
+type MacButton struct {
+	Button
 }
 
 func main() {
+	ui := getUI("win")
+	button := ui.CreateButton()
+	println(button.getHeight())
 }
