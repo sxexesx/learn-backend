3fbd5b344af1a85b88d25f93ed51099cf128a8c4
sxexesx <sxexesx@gmail.com>
2025-10-30 13:47:25 +0300

    work commit

--- go/_projects/06_struct/main.go
+++ go/_projects/06_struct/main.go
@@ -1,12 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+// Пример разницы с вызовом по ссылки и по значению.
+// В первом варианте (с Point) setX изменит исходный объект point
+// Во втором - нет
 
 func main() {
 	point := Point{1, 1}
 
 	point.SetX(100)
 	fmt.Println(point)
+
+	p := Person{
+		Name:    "Igor",
+		Surname: "Starshinov",
+	}
+	p.UpPerson()
+
+	fmt.Printf("%#+v", p)
 }
 
 type Point struct {
@@ -16,4 +31,14 @@ type Point struct {
 
 func (p *Point) SetX(v int) {
 	p.X = v
+}
+
+type Person struct {
+	Name    string
+	Surname string
+}
+
+func (p Person) UpPerson() {
+	p.Name = strings.ToUpper(p.Name)
+	p.Surname = strings.ToUpper(p.Surname)
 }
