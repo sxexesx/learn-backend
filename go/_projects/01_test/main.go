d0213a179395ea891b0235326f2caf023cf6475b
sxexesx <sxexesx@gmail.com>
2026-01-22 17:12:41 +0300

    remove go

--- go/_projects/01_test/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	var rr rune
-	fmt.Printf("Hello, World! %c", rr)
-}
--- go/_projects/08_modules/server/pkg/storage/storage.go
+++
@@ -1,7 +0,0 @@
-package storage
-
-import "github.com/sxexesx/learn-backend/go/_projects/08_modules/server/internal/storage"
-
-func NewStorage() *storage.Storage {
-	return storage.NewStorage()
-}
